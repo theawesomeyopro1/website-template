f6f7362104c800dcd66206ec0669fe2b4670df0f
theawesomeyopro <108738248+theawesomeyopro1@users.noreply.github.com>
2024-06-20 22:18:19 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// how to print hello world in java script, click, ctrl + shift + I to open the inspect page and click the console tab to show the "console works" message.
+console.log('console works!')
+
+
+//this is a comment
+// uncomment the next line by removing the forward slashes so it shows an alert
+//window.alert('this is an alert')
+
+// this is how functions work in javascript
+// Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
